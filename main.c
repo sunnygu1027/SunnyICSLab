de846866b0a52a9aa331fb0a0e3e63bf5e6302e8
sunnygu1027 <376143708@qq.com>
2026-09-20 11:37:45 +0800

    Modified version

--- main.c
+++ main.c
@@ -2,10 +2,6 @@
 
 int main()
 {
-<<<<<<< HEAD
-    printf("Happy National D\n");// @TODO: print a sentence you want.
-=======
     printf("Happy National!!!\n");// @TODO: print a sentence you want.
->>>>>>> feature
     printf("Hello, world!\n");
 }
